93191df0ca0e6f316bf8f2ea6773f3fec99c2357
ALex Zhao <alex.zhao.superman@gmail.com>
2020-03-17 11:17:44 -0700

    Got rid of excess spaces, combined lines for readability

--- AVLTree.py
+++ AVLTree.py
@@ -1,16 +1,16 @@
 # Python code to insert a node in AVL tree 
   
-# Generic tree node class 
+# Generic tree node class
 class TreeNode(object): 
-    def __init__(self, val): 
+    def __init__(self, val):
         self.val = val 
         self.left = None
         self.right = None
         self.height = 0
   
-# AVL tree class which supports the  
+# AVL tree class which supports the
 # Insert operation 
-class AVL_Tree(object): 
+class AVL_Tree(object):
     """
     Recursive function to insert key in  subtree rooted with node and returns 
     new root of subtree. 
@@ -20,7 +20,7 @@ class AVL_Tree(object):
         Will return new root and do all recursive balancing/insertion.
         """
         # Step 1 - Perform normal BST
-        if not root: 
+        if not root:
             return TreeNode(key)
         elif key < root.val:
             root.left = self.insert(root.left, key)
@@ -34,83 +34,79 @@ class AVL_Tree(object):
 
         # Step 3 - Get the balance factor
         balance = self.getBalance(root)
-  
+
         # Step 4 - If the node is unbalanced,
-        # then try out the 4 cases 
-        # Case 1 - Left Left 
-        if balance > 1 and key < root.left.val: 
-            return self.rightRotate(root) 
-  
-        # Case 2 - Right Right 
-        if balance < -1 and key > root.right.val: 
-            return self.leftRotate(root) 
-  
-        # Case 3 - Left Right 
-        if balance > 1 and key > root.left.val: 
-            root.left = self.leftRotate(root.left) 
-            return self.rightRotate(root) 
-  
-        # Case 4 - Right Left 
-        if balance < -1 and key < root.right.val: 
-            root.right = self.rightRotate(root.right) 
-            return self.leftRotate(root) 
-  
-        return root 
-  
-    def leftRotate(self, z): 
-  
+        # then try out the 4 cases
+        # Case 1 - Left Left
+        if balance > 1 and key < root.left.val:
+            return self.rightRotate(root)
+
+        # Case 2 - Right Right
+        if balance < -1 and key > root.right.val:
+            return self.leftRotate(root)
+
+        # Case 3 - Left Right
+        if balance > 1 and key > root.left.val:
+            root.left = self.leftRotate(root.left)
+            return self.rightRotate(root)
+
+        # Case 4 - Right Left
+        if balance < -1 and key < root.right.val:
+            root.right = self.rightRotate(root.right)
+            return self.leftRotate(root)
+
+        return root
+
+    def leftRotate(self, z):
+
         y = z.right
         T2 = y.left
-  
-        # Perform rotation 
-        y.left = z 
-        z.right = T2 
-  
-        # Update heights 
-        z.height = 1 + max(self.getHeight(z.left), 
-                         self.getHeight(z.right)) 
-        y.height = 1 + max(self.getHeight(y.left), 
-                         self.getHeight(y.right)) 
+
+        # Perform rotation
+        y.left = z
+        z.right = T2
+
+        # Update heights
+        z.height = 1 + max(self.getHeight(z.left), self.getHeight(z.right))
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right)) 
   
         # Return the new root 
         return y 
-  
+
     def rightRotate(self, z):
+
+        y = z.left
+        T3 = y.right
+
+        # Perform rotation
+        y.right = z
+        z.left = T3
+
+        # Update heights
+        z.height = 1 + max(self.getHeight(z.left), self.getHeight(z.right)) 
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right)) 
   
-        y = z.left 
-        T3 = y.right 
-  
-        # Perform rotation 
-        y.right = z 
-        z.left = T3 
-  
-        # Update heights 
-        z.height = 1 + max(self.getHeight(z.left), 
-                        self.getHeight(z.right)) 
-        y.height = 1 + max(self.getHeight(y.left), 
-                        self.getHeight(y.right)) 
-  
-        # Return the new root 
-        return y 
+        # Return the new root
+        return y
 
     @staticmethod
-    def getHeight(root): 
-        if not root: 
+    def getHeight(root):
+        if not root:
             return -1
-  
-        return root.height 
 
-    def getBalance(self, root): 
-        if not root: 
+        return root.height
+
+    def getBalance(self, root):
+        if not root:
             return -1
   
-        return self.getHeight(root.left) - self.getHeight(root.right) 
-  
-    def preOrder(self, root): 
-  
-        if not root: 
+        return self.getHeight(root.left) - self.getHeight(root.right)
+
+    def preOrder(self, root):
+
+        if not root:
             return
-  
-        print("{0} ".format(root.val)) 
-        self.preOrder(root.left) 
+
+        print("{0} ".format(root.val))
+        self.preOrder(root.left)
         self.preOrder(root.right)
